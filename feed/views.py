473d373ff2fe604163fa29053a4f89ab7a0e8a7b
Jim Hessin <jhessin@gmail.com>
2023-08-21 18:06:48 -0600

    Mon Aug 21 18:06:52 2023

--- feed/views.py
+++ feed/views.py
@@ -3,3 +3,10 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['my_thing'] = 'Hello world :P this is dynamic'
+
+        return context
